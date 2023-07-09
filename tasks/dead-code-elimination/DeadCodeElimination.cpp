bec060c3c9411f97f5c99585277c69887031a5b5
perinze <perinzefaper@foxmail.com>
2023-07-09 18:58:54 +0800

    implementing

--- tasks/dead-code-elimination/DeadCodeElimination.cpp
+++ tasks/dead-code-elimination/DeadCodeElimination.cpp
@@ -5,7 +5,9 @@
 #include "llvm/IR/InstVisitor.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
+
 #include "llvm/Transforms/Utils/Local.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
 
 #define DEBUG_TYPE "remove-dead-code"
 
@@ -13,27 +15,74 @@ using namespace llvm;
 
 namespace {
     struct DeadCodeEliminationPass : PassInfoMixin<DeadCodeEliminationPass> {
-        PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM) {
-            errs() << "Running DeadCodeEliminationPass on function " << F.getName() << "\n";
+        static void deleteTriviallyDeadInstruction(Function &F) {
+            errs() << "Delete trivially dead instructions in " << F.getName() << "\n";
 
+            // TODO: change to reverse approach
+            // TODO: iterate basic blocks
             bool local_changed;
             do {
                 local_changed = false;
                 auto dead_inst = std::set<Instruction*>();
-                for (auto I = inst_begin(F); I != inst_end(F); ++I) {
+                for (auto I = inst_begin(F), E = inst_end(F); I != E; ++I) {
                     auto inst = &(*I);
                     if (isInstructionTriviallyDead(inst)) {
                         dead_inst.insert(inst);
                     }
                 }
                 for (auto inst : dead_inst) {
-                    RecursivelyDeleteTriviallyDeadInstructions(inst);
+                    local_changed = true;
+                    inst->eraseFromParent();
                 }
             } while (local_changed);
+
+            errs() << "Done\n";
+        }
+
+        static void simplifyConditionalBranch(Function &F) {
+            errs() << "Simplifying conditional branches that always branch to the same block\n";
+            for (auto I = inst_begin(F); I != inst_end(F); ++I) {
+                auto inst = &(*I);
+                if (auto *BI = dyn_cast<BranchInst>(inst)) {
+                    if (BI->getNumSuccessors() == 2) {
+                        errs() << *BI << " has 2 successors\n";
+                        if (BI->getSuccessor(0) == BI->getSuccessor(1)) {
+                            errs() << *BI << "'s successors branch to the same basic blocks\n";
+                            auto newBI = BranchInst::Create(BI->getSuccessor(0));
+                            ReplaceInstWithInst(BI, newBI);
+                            errs() << "Replaced with unconditional branch instruction\n";
+                        }
+                    }
+                }
+            }
+            errs() << "Done\n";
+        }
+
+        static void simplifySingleBranchBlock(Function &F) {
+            errs() << "Blocks that just contain a single unconditional branch instruction to the next block\n";
+            for (auto BBI = F.begin(), E = F.end(); BBI != E; ++BBI) {
+                auto instList = BBI->getInstList();
+            }
+            errs() << "Done\n";
+        }
+
+        static void simplifyBasicBlock(Function &F) {
+            errs() << "Simplifying basic blocks in " << F.getName() << "\n";
+            simplifySingleBranchBlock(F);
+            simplifyConditionalBranch(F);
+            errs() << "Done\n";
+        }
+
+        PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM) {
+            errs() << "Running DeadCodeEliminationPass on function " << F.getName() << "\n";
+
+            deleteTriviallyDeadInstruction(F);
+            simplifyBasicBlock(F);
 
             return PreservedAnalyses::none();
         }
     };
+
 } // namespace
 
 /// Registration
